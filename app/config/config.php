181bf92c0bdcb92bce6d56f45169071c28474650
Rhobrenz <buenavistarhobrenz@gmail.com>
2018-09-06 19:27:56 +0800

    MVC

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', '_YOUR_USER');
+define('DB_PASS', '_YOUR_PASS');
+define('DB_NAME', '_YOUR_DBNAME');
+
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '_YOUR_URL_');
+  // Site Name
+  define('SITENAME', '_YOUR_SITENAME_');
